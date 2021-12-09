a67be02a9111f13f707de7c01aee89ff32acc057
Mark <mark.j.wee@gmail.com>
2021-12-09 12:32:17 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // Write your code here
-function multiply(num1, num2) {
-    if (num1.parsefloat() * num2.parsefloat() == 62)
-    return console.log(num1, num2)
-}
+// function multiply(num1, num2) {
+//     let num1 * num2 = 62
+//     return console.log("works")
+// }
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-function random() {
 
-}
+let random = (Math.floor(Math.random(parseInt) * 10) + 1);
+
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4
+
+
+let max = Math.max(0, 1 , 2, 3, 20)
